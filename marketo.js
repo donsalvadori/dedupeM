c0946895cfccb959ef03c56b904043c5a3cb2db3
Chris Salvi <csalvi42@gmail.com>
2018-07-09 10:16:39 -0700

    almosted finished code need to create simple interface

---
+++ marketo.js
@@ -0,0 +1,129 @@
+// need to write basic input mechanism with node to input data
+// need to sort by date
+// if dates are identical data provided last in the list should be preferred
+
+
+// const fs = require('fs');
+
+// fs.readFile('leads.json', (err, data) => {  
+//     if (err) throw err;
+//     let json = JSON.parse(data);
+//     console.log(json);
+// });
+
+// console.log('This is after the read call');  
+
+const json = {
+	"leads":[
+		{
+		"_id": "jkj238238jdsnfsj23",
+		"email": "foo@bar.com",
+		"firstName":  "John",
+		"lastName": "Smith",
+		"address": "123 Street St",
+		"entryDate": "2014-05-07T17:30:20+00:00"
+		},
+		{
+		"_id": "edu45238jdsnfsj23",
+		"email": "mae@bar.com",
+		"firstName":  "Ted",
+		"lastName": "Masters",
+		"address": "44 North Hampton St",
+		"entryDate": "2014-05-07T17:31:20+00:00"
+		},
+		{
+		"_id": "wabaj238238jdsnfsj23",
+		"email": "bog@bar.com",
+		"firstName":  "Fran",
+		"lastName": "Jones",
+		"address": "8803 Dark St",
+		"entryDate": "2014-05-07T17:31:20+00:00"
+		},
+		{
+		"_id": "jkj238238jdsnfsj23",
+		"email": "coo@bar.com",
+		"firstName":  "Ted",
+		"lastName": "Jones",
+		"address": "456 Neat St",
+		"entryDate": "2014-05-07T17:32:20+00:00"
+		},
+		{
+		"_id": "sel045238jdsnfsj23",
+		"email": "foo@bar.com",
+		"firstName":  "John",
+		"lastName": "Smith",
+		"address": "123 Street St",
+		"entryDate": "2014-05-07T17:32:20+00:00"
+		},
+		{
+		"_id": "qest38238jdsnfsj23",
+		"email": "foo@bar.com",
+		"firstName":  "John",
+		"lastName": "Smith",
+		"address": "123 Street St",
+		"entryDate": "2014-05-07T17:32:20+00:00"
+		},
+		{
+		"_id": "vug789238jdsnfsj23",
+		"email": "foo1@bar.com",
+		"firstName":  "Blake",
+		"lastName": "Douglas",
+		"address": "123 Reach St",
+		"entryDate": "2014-05-07T17:33:20+00:00"
+		},
+		{
+		"_id": "wuj08238jdsnfsj23",
+		"email": "foo@bar.com",
+		"firstName":  "Micah",
+		"lastName": "Valmer",
+		"address": "123 Street St",
+		"entryDate": "2014-05-07T17:33:20+00:00"
+		},
+		{
+		"_id": "belr28238jdsnfsj23",
+		"email": "mae@bar.com",
+		"firstName":  "Tallulah",
+		"lastName": "Smith",
+		"address": "123 Water St",
+		"entryDate": "2014-05-07T17:33:20+00:00"
+		},
+		{
+		"_id": "jkj238238jdsnfsj23",
+		"email": "bill@bar.com",
+		"firstName":  "John",
+		"lastName": "Smith",
+		"address": "888 Mayberry St",
+		"entryDate": "2014-05-07T17:33:20+00:00"
+		}
+	]
+}
+
+
+const leads = json.leads;
+
+
+ 
+const uniqueID = [...new Set(leads.map( item => item._id ))];
+const uniqueEmail = [...new Set(leads.map( item => item.email ))];
+
+
+const jsonFiltered = uniqueID.map( id => leads.find( obj => (id === obj._id) ) );
+const emailFilter = uniqueEmail.map( email => jsonFiltered.find( obj => (email === obj.email) ) );
+
+
+const solution = emailFilter.filter( obj =>  obj !== undefined ); 
+
+
+const datesArr = solution.map( obj => obj.entryDate );
+
+const noDuplicates = datesArr.map( (el, i) => {
+	if(i + 1 === i) {
+		delete datesArr[i];
+	}
+});
+
+console.log(datesArr);
+
+console.log(noDuplicates);
+
+
